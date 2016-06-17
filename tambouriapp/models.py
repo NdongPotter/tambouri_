e1524f1549f4de92f36e9001dd29971b84fe6d0b
Amadu <amadu@MacBook-Pro-de-Amadu.local>
2016-06-17 12:33:04 +0000

    settings db and tambouriapp

---
+++ tambouriapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Voiture(models.Model):
+    #modele
+    #marque
+    #annee
+    #photo
+    #description
+    #prix de vente
+    pass
+
+class Villa(models.Model):
+    #localisation
+    #numero
+    #description
+    #photo
+    #prix de vente
+    pass
+
+class Tableau(models.Model):
+    #prix
+    #peintre
+    #description
+    #prix de vente
+    #dimension
